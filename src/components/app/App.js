bc1f6e0ce18dd4d9f7b1db8a2871f5c43f57d798
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-13 13:10:10 -0400

    Add basic outuput of todo list items

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import todoList from '../../mocks/todo-list';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
+import TodoItem from '../todo-item/TodoItem';
 
 function App() {
   return (
-    <h1>Test</h1>
+    <>
+      <h1 className="container">TODO APP</h1>
+      <ListGroup>
+        {todoList.map(({ id, title, detail, isComplete }) => (
+          <ListGroupItem key={id}>
+            <TodoItem
+              title={title}
+              detail={detail}
+              isComplete={isComplete}
+            />
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    </>
   );
 }
 
