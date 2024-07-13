2627bde995a09aa39aa8b2dec4ab2ae109d9ce77
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-13 14:06:17 -0400

    Add styling to make better ui/ux

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,16 +4,19 @@ import todoList from '../../mocks/todo-list';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Container from 'react-bootstrap/Container';
 
 import TodoItem from '../todo-item/TodoItem';
 
 function App() {
   return (
-    <>
-      <h1 className="container">TODO APP</h1>
-      <ListGroup>
+    <Container className="d-flex flex-column align-items-center justify-content-center p-3 p-sm-4 p-md-5">
+      <h1 className="mb-4 mb-sm-5">ToDo App</h1>
+      <ListGroup className="gap-4">
         {todoList.map(({ id, title, detail, isComplete }) => (
-          <ListGroupItem key={id}>
+          <ListGroupItem
+            className="d-flex flex-column flex-sm-row gap-1 gap-sm-3 gap-md-5 align-items-center justify-content-between border border-secondary rounded shadow p-3 p-sm-4"
+            key={id}>
             <TodoItem
               title={title}
               detail={detail}
@@ -22,7 +25,7 @@ function App() {
           </ListGroupItem>
         ))}
       </ListGroup>
-    </>
+    </Container>
   );
 }
 
--- src/components/todo-item/TodoItem.js
+++ src/components/todo-item/TodoItem.js
@@ -4,18 +4,22 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 function TodoItem({ title, detail, isComplete }) {
   return (
     <>
-      <p>{title}</p>
-      <p>{detail}</p>
-      <p>{isComplete ? 'completed' : 'pending'}</p>
-      <Button variant="primary">
-        <i className="bi bi-pencil-square"></i>
-      </Button>
-      <Button variant="danger">
-        <i className="bi bi-trash"></i>
-      </Button>
-      <Button variant="success">
-        <i className="bi bi-check2-square"></i>
-      </Button>
+      <div className="text-center text-sm-start">
+        <h2>{title}</h2>
+        <p>{detail}</p>
+        {/* <p>{isComplete ? 'completed' : 'pending'}</p> */}
+      </div>
+      <div className="d-flex gap-2">
+        <Button variant="primary">
+          <i className="bi bi-pencil-square"></i>
+        </Button>
+        <Button variant="danger">
+          <i className="bi bi-trash"></i>
+        </Button>
+        <Button variant="success">
+          <i className="bi bi-check2-square"></i>
+        </Button>
+      </div>
     </>
   );
 }
