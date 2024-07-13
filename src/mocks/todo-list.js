01d0ff4fe36125301a6e6ebe376a012177c80eb6
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-13 09:39:32 -0400

    Add mock todo list

---
+++ src/mocks/todo-list.js
@@ -0,0 +1,22 @@
+const todoList = [
+  {
+    id: 0,
+    title: 'Buy groceries',
+    detail: 'Milk, eggs, bananas',
+    isComplete: false,
+  },
+  {
+    id: 1,
+    title: 'Walk a dog',
+    detail: 'Need that 10k steps a day!',
+    isComplete: true,
+  },
+  {
+    id: 2,
+    title: 'Watch Netflix',
+    detail: 'The House of Dragons is on',
+    isComplete: false,
+  },
+];
+
+export default todoList;
