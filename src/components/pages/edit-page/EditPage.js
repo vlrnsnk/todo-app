ba599514a240f4cefc5ba74d45652df361c25c39
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-14 14:29:28 -0400

    Implement correct filling of edit task form by task id

--- src/components/pages/edit-page/EditPage.js
+++ src/components/pages/edit-page/EditPage.js
@@ -1,13 +1,23 @@
+import { useParams } from "react-router-dom";
+
+import todoList from '../../../mocks/todo-list';
+
 import Header from "../../header/Header";
 import EditTaskForm from "../../edit-task-form/EditTaskForm";
 
-function EditPage({ title, detail }) {
+function EditPage() {
+  const { id } = useParams();
+  
+  const currentTask = todoList.filter((todoItem) => todoItem.id === Number(id));
+
+  const { title, detail } = currentTask[0];
+
   return (
     <>
       <Header text="Edit Task" isGoBack={true}/>
       <EditTaskForm
-        title="Test edit title"
-        detail="Test edit detail"
+        title={title}
+        detail={detail}
       />
     </>
   );
