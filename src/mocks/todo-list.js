e253f6d0b13be60c0fa410c0b74059ee8ac0d2a6
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-14 15:37:20 -0400

    Change mock tasks ids to start from 1 to be in line with sql id generation

--- src/mocks/todo-list.js
+++ src/mocks/todo-list.js
@@ -1,18 +1,18 @@
 const todoList = [
   {
-    id: 0,
+    id: 1,
     title: 'Buy groceries',
     detail: 'Milk, eggs, bananas',
     isComplete: false,
   },
   {
-    id: 1,
+    id: 2,
     title: 'Walk a dog',
     detail: 'Need that 10k steps a day!',
     isComplete: true,
   },
   {
-    id: 2,
+    id: 3,
     title: 'Watch Netflix',
     detail: 'The House of Dragons is on',
     isComplete: false,
