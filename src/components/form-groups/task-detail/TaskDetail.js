c8d618bc450c85d19ec6ce07d2772e01e675f579
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-14 10:00:24 -0400

    Add state to inputs of form groups

--- src/components/form-groups/task-detail/TaskDetail.js
+++ src/components/form-groups/task-detail/TaskDetail.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
 import FormGroup from 'react-bootstrap/FormGroup';
 import FormLabel from 'react-bootstrap/FormLabel';
 import FormControl from 'react-bootstrap/FormControl';
 
 function TaskDetailFormGroup({ detail }) {
+  const [taskDetail, setTaskDetail] = useState(detail ?? '');
+
   return (
     <FormGroup className="mb-3 mb-sm-4" controlId="formTaskDetail">
       <FormLabel>Task detail</FormLabel>
-      <FormControl type="text" placeholder="Detail" size="lg" value={detail} />
+      <FormControl
+        type="text"
+        placeholder={detail ? '' : 'Detail'}
+        size="lg"
+        value={taskDetail}
+        onChange={(event) => setTaskDetail(event.target.value)}
+      />
     </FormGroup>
   );
 }
--- src/components/form-groups/task-title/TaskTitle.js
+++ src/components/form-groups/task-title/TaskTitle.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
 import FormGroup from 'react-bootstrap/FormGroup';
 import FormLabel from 'react-bootstrap/FormLabel';
 import FormControl from 'react-bootstrap/FormControl';
 
 function TaskTitleFormGroup({ title }) {
+  const [taskTitle, setTaskTitle] = useState(title ?? '');
+
   return (
     <FormGroup className="mb-3 mb-sm-4" controlId="formTaskTitle">
       <FormLabel>Task title</FormLabel>
-      <FormControl type="text" placeholder="Title" required size="lg" value={title} />
+      <FormControl
+        type="text"
+        placeholder={title ? '' : 'Title'}
+        required
+        size="lg"
+        value={taskTitle}
+        onChange={(event) => setTaskTitle(event.target.value)}
+      />
     </FormGroup>
   );
 }
