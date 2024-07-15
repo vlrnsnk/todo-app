2febd3db6eaa26f005153e4d16b0ac464e0aa8c5
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-15 18:32:49 -0400

    Make and apply migrations

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -7,6 +7,6 @@ def home_view(request):
 
 def todo_detail_view(request, todo_id):
     obj = Task.objects.get(id=todo_id)
-    return HttpResponse("<h1>test</h1>")
-    # return HttpResponse(f"<h1>Title: {obj.title}, Detail: {obj.detail}</h1>")
+    # return HttpResponse("<h1>test</h1>")
+    return HttpResponse(f"<h1>Title: {obj.title}, Detail: {obj.detail}</h1>")
 
