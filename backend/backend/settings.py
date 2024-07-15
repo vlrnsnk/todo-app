19fdf231541f3342ab204714eded42c5c2ff117c
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-14 21:28:30 -0400

    Add "rest_framework" and "todo" to INSTALLED_APPS and supabase settings to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.mnqojempzkivrxsnqigz',
+        'PASSWORD': 'vecbix-zutfA8-farmaw',
+        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
