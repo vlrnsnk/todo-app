9bf5b8b844bcd32215343a6f78f15dcc50e131ea
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-15 23:01:51 -0400

    Implement adding tasks and output messages if not successful

--- src/components/add-task-form/AddTaskForm.js
+++ src/components/add-task-form/AddTaskForm.js
@@ -1,4 +1,4 @@
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios';
 
@@ -9,43 +9,63 @@ import TaskTitleFormGroup from '../form-groups/task-title/TaskTitle';
 import TaskDetailFormGroup from '../form-groups/task-detail/TaskDetail';
 
 function AddTaskForm() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
 
-  const handleSubmit = async () => {
+  const [hasTaskBeenAdded, setHasTaskBeenAdded] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
     await axios.post('http://127.0.0.1:8000/api/tasks/', {
       "title": title,
       "detail": detail
     })
     .then((response) => {
       console.log(response);
-      // navigate("");
+      setHasTaskBeenAdded(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
     })
     .catch((error) => {
+      setHasTaskBeenAdded(false);
       console.log(error);
     });
   };
 
   return (
-    <Form className="w-75 w-sm-50" onSubmit={handleSubmit}>
-      <TaskTitleFormGroup
-        title={title}
-        handleChange={(event) => setTitle(event.target.value)}
-      />
-      <TaskDetailFormGroup
-        detail={detail}
-        handleChange={(event) => setDetail(event.target.value)}
-      />
-      <Button
-        className="w-100"
-        size="lg"
-        variant="success"
-        type="submit"
-      >
-        Add
-      </Button>
-    </Form>
+    <>
+      {hasTaskBeenAdded ?
+        <p className="fs-3 text-success">Task has been added</p> :
+        <Form className="w-75 w-sm-50" onSubmit={handleSubmit}>
+          <TaskTitleFormGroup
+            title={title}
+            handleChange={(event) => setTitle(event.target.value)}
+          />
+          <TaskDetailFormGroup
+            detail={detail}
+            handleChange={(event) => setDetail(event.target.value)}
+          />
+          <Button
+            className="w-100"
+            size="lg"
+            variant="success"
+            type="submit"
+          >
+            Add
+          </Button>
+          {hasTaskBeenAdded === false ?
+            <p className="fs-3 text-danger text-center mt-3 mt-md-4 mt-lg-5">
+              There was an error adding your task
+            </p> :
+            null
+          }
+        </Form>
+      }
+    </>
   );
 }
 
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -33,7 +33,6 @@ function App() {
     };
 
     fetchTasks();
-    // setTasks(todoList);
   }, []);
 
   return (
