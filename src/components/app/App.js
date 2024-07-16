0bffc99d11ec5e7ebebfbb7f4acc62f44c3920a0
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-15 20:31:25 -0400

    Install axios and add useState to App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import todoList from '../../mocks/todo-list';
@@ -10,14 +11,32 @@ import PageWrapper from '../layout/page-wrapper/PageWrapper';
 import MainPage from '../pages/main-page/MainPage';
 import AddPage from '../pages/add-page/AddPage';
 import EditPage from '../pages/edit-page/EditPage';
+import axios from 'axios';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      await axios.get('http://127.0.0.1:8000/api/tasks/')
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    fetchTasks();
+    // setTasks(todoList);
+  }, []);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
         <Route path={AppRoutes.MAIN} element={<PageWrapper />}>
-          <Route index element={<MainPage todoList={todoList} />} />
+          <Route index element={<MainPage todoList={tasks} />} />
           <Route path={AppRoutes.ADD} element={<AddPage />} />
           <Route path={AppRoutes.EDIT} element={<EditPage />} />
         </Route>
