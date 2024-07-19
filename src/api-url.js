15b53bf27dc9d41a75c634c2654e6da09bb76ead
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-18 21:58:17 -0400

    Add commented out private api endpoint

--- src/api-url.js
+++ src/api-url.js
@@ -1,3 +1,7 @@
-const apiUrl = 'http://3.143.143.23:8000/api/tasks/'
+// Public api
+const apiUrl = 'http://3.143.143.23:8000/api/tasks/';
+
+// Private api
+// const apiUrl = "http://135.0.137.131:8000/api/tasks/";
 
 export default apiUrl;
