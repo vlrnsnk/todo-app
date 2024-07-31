09c0dc116bc45c5879c3241bdbe44a21ed64466e
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2024-07-30 22:25:56 -0400

    Change public apiUrl to railway

--- src/api-url.js
+++ src/api-url.js
@@ -1,5 +1,5 @@
 // Public api
-const apiUrl = 'http://3.143.143.23:8000/api/tasks/';
+const apiUrl = 'https://task-maestro-backend-production.up.railway.app/api/tasks/';
 
 // Private api
 // const apiUrl = "http://135.0.137.131:8000/api/tasks/";
